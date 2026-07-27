e43f774c929b598c5cf9b174a295b31543391977
aroyamounota205 <251-15-205@diu.edu.bd>
2026-07-28 04:22:29 +0600

    week 1 problem 3

--- ifElse.java
+++ ifElse.java
@@ -3,7 +3,6 @@ public class ifElse {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
-
         if(a % 2 != 0){
             System.out.println("Weird");
         }
